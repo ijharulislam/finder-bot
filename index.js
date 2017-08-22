a1d78ffcc8e665e08c829a5015d2aae6baade5c5
ijhar <ijharalkawsary@gmail.com>
2017-08-22 11:53:09 +0600

    Removing comments

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-//
-// This is main file containing code implementing the Express server and functionality for the Express echo bot.
-//
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +5,6 @@ const request = require('request');
 const path = require('path');
 var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
 
-// The rest of the code implements the routes for our Express server.
 let app = express();
 
 app.use(bodyParser.json());
@@ -16,7 +12,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// Webhook validation
+
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
@@ -28,27 +24,23 @@ app.get('/webhook', function(req, res) {
   }
 });
 
-// Display the web page
+
 app.get('/', function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(messengerButton);
   res.end();
 });
 
-// Message processing
 app.post('/webhook', function (req, res) {
   console.log(req.body);
   var data = req.body;
 
-  // Make sure this is a page subscription
   if (data.object === 'page') {
-    
-    // Iterate over each entry - there may be multiple if batched
+
     data.entry.forEach(function(entry) {
       var pageID = entry.id;
       var timeOfEvent = entry.time;
 
-      // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
         if (event.message) {
           receivedMessage(event);
@@ -59,17 +51,10 @@ app.post('/webhook', function (req, res) {
         }
       });
     });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
     res.sendStatus(200);
   }
 });
 
-// Incoming events handling
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -86,8 +71,6 @@ function receivedMessage(event) {
   var messageAttachments = message.attachments;
 
   if (messageText) {
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the template example. Otherwise, just echo the text we received.
     switch (messageText) {
       case 'generic':
         sendGenericMessage(senderID);
@@ -106,21 +89,13 @@ function receivedPostback(event) {
   var recipientID = event.recipient.id;
   var timeOfPostback = event.timestamp;
 
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
   var payload = event.postback.payload;
 
   console.log("Received postback for user %d and page %d with payload '%s' " + 
     "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
   sendTextMessage(senderID, "Postback called");
 }
 
-//////////////////////////
-// Sending helpers
-//////////////////////////
 function sendTextMessage(recipientId, messageText) {
   var messageData = {
     recipient: {
@@ -203,7 +178,6 @@ function callSendAPI(messageData) {
   });  
 }
 
-// Set Express to listen out for HTTP requests
 var server = app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port %s", server.address().port);
 });
